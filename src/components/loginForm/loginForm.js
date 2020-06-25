3b7a0e2ebfc7da8f9a14f3c0ddbe261ba573ada9
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-06-25 19:52:55 +0200

    validate login

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -5,7 +5,9 @@ import SubmitButton from "../submitButton/submitButton";
 import "./loginForm.scss";
 
 const LoginForm = () => {
-    const [account, setAccount] = useState({ user: "", password: "" });
+    const [account, setAccount] = useState({ user: "", psw: "" });
+    const [mailError, setMailError] = useState("");
+    const [pswError, setPswError] = useState("");
 
     const accountChange = (e) => {
         setAccount({
@@ -14,15 +16,50 @@ const LoginForm = () => {
         });
     };
 
+    const validMail = () => {
+        if (
+            account.user &&
+            !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                account.user
+            )
+        ) {
+            setMailError("Mail no válido");
+            return false;
+        } else if (!account.user) {
+            setMailError("Mail requerido");
+            return false;
+        } else {
+            setMailError("");
+            return true;
+        }
+    };
+
+    const validPsw = () => {
+        if (account.psw && !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(account.psw)) {
+            setPswError(
+                "La contraseña debe contener 8 carácteres mínimo, una mayúscula, una minúscula y un número"
+            );
+            return false;
+        } else if (!account.psw) {
+            setPswError("Contraseña requerida");
+            return false;
+        } else {
+            setPswError("");
+            return true;
+        }
+    };
+
     const logIn = (e) => {
-        if (e) e.preventDefault();
+        e.preventDefault();
+        const mail = validMail();
+        const psw = validPsw();
     };
 
     return (
         <div className="formWrapper">
             <p className="loginTxt">Login</p>
             <form className="loginForm" onSubmit={logIn}>
-                <div className="inputWrapper space">
+                <div className="inputWrapper center">
                     <span className="svg">
                         <MdMail />
                     </span>
@@ -34,20 +71,22 @@ const LoginForm = () => {
                         placeholder="username"
                     ></input>
                 </div>
-                <div className="inputWrapper space">
+                <div className="loginError">{mailError}</div>
+                <div className="inputWrapper center space">
                     <span>
                         <BsLockFill />
                     </span>
                     <input
                         className="input"
-                        name="password"
+                        name="psw"
                         type="password"
-                        value={account.password}
+                        value={account.psw}
                         onChange={accountChange}
                         placeholder="password"
                     ></input>
                 </div>
-                <div className="loginBtnWrapper">
+                <div className="loginError">{pswError}</div>
+                <div className="loginBtnWrapper space">
                     <SubmitButton txt="login" />
                 </div>
                 <div className="space recover">
