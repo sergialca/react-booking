b52d8910f8b9f391579ae581626df361988f7730
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-06-29 12:04:44 +0200

    Updates

---
+++ precache-manifest.6d1b5da02e6c87be8e74c08a7b730fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec165d29963da796199bf304d8fd510",
+    "url": "/react-booking/index.html"
+  },
+  {
+    "revision": "eef06aa4c94a707cf69a",
+    "url": "/react-booking/static/css/main.09a075c1.chunk.css"
+  },
+  {
+    "revision": "39bc9b65031eaf84ed36",
+    "url": "/react-booking/static/js/2.5d728257.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/react-booking/static/js/2.5d728257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef06aa4c94a707cf69a",
+    "url": "/react-booking/static/js/main.56580012.chunk.js"
+  },
+  {
+    "revision": "e5db6a9f93b8914aa405",
+    "url": "/react-booking/static/js/runtime-main.82cb330d.js"
+  }
+]);
