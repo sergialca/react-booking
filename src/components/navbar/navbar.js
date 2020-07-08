46d163802351d9731f0f8bc2692a098ec263284d
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-07-08 17:34:28 +0200

    no bootstrap

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,21 +1,8 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 import "./navbar.scss";
 
 const NavBar = () => {
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Silock</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#features">Reservar</Nav.Link>
-                    <Nav.Link href="#pricing">Mis Reservas</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+    return <div>HOLA</div>;
 };
 
 export default NavBar;
